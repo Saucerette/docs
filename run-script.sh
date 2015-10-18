52f8b5a5b1340832042e412f2f634e9c4df3f52d
Tim Rodger <tim.rodger@gmail.com>
2015-10-18 12:49:32 +0100

    Create run-script.sh

---
+++ run-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run a glue script
+# remember the content-type header!
+
+script=$1
+glue=http://192.168.59.103:49400/
+
+curl  -X POST  -d @$script  -H 'Content-Type: text/plain' $glue -v
